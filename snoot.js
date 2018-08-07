2bf2bcfdb143d521195df16f3105e8595693fe56
Unknown <jhample323@west-mec.org>
2018-08-06 18:25:23 -0700

    First Commit
    Added some JavaScript for the excercise

---
+++ snoot.js
@@ -0,0 +1,11 @@
+/*
+      
+      snoot.js
+      Form Validation functions for snoot.html
+      
+      Author: Jaggar Hample
+      Date: 6 August 2018
+      
+*/
+
+alert("Hello, I am linked!");
