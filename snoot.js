11195cfa41a51c91b88ca82a13d3fb46c158a379
Unknown <jhample323@west-mec.org>
2018-08-07 18:25:58 -0700

    Dates
    Worked on the dates for form validation. For example the leap years and other issues with different days in  months

--- snoot.js
+++ snoot.js
@@ -8,4 +8,60 @@
       
 */
 
-alert("Hello, I am linked!");
+"use strict";
+
+var twentyNine = document.createDocumentFragment();
+var thirty = document.createDocumentFragment();
+var thirtyOne = document.createDocumentFragment();
+
+// function to turn off select list defaults
+function removeSelectDefaults() {
+    var emptyBoxes = document.getElementsByTagName("select");
+    for (var i = 0; i < emptyBoxes.length; i++) {
+        emptyBoxes[i].selectedIndex = -1;
+    }
+}
+
+// function to set up document fragments for days of month
+function setUpDays() {
+    // get the days option tags
+    var dates = document.getElementById("delivDy").getElementsByTagName("option");
+    twentyNine.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[28].cloneNode(true));
+    thirty.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[28].cloneNode(true));
+    thirtyOne.appendChild(dates[29].cloneNode(true));
+    thirtyOne.appendChild(dates[30].cloneNode(true));
+}
+
+// function to update the days select list
+function updateDays() {
+    var deliveryDay = document.getElementById("delivDy");
+    var dates = deliveryDay.getElementsByTagName("option");
+    var deliveryMonth = document.getElementById("delivyMo");
+    var deliveryYear = document.getElementById("delivYr");
+    var selectedMonthc = deliveryMonth.options[deliveryMonth.selectedIndex].value;
+    while (dates[28]) {
+        deliveryDay.removeChild(dates[28]);
+    }
+    if (deliveryMonth.selectedIndex === -1) {
+        deliveryMonth.selectedIndex = 0;
+    }
+    // if Febuary and 2020 - leap year - twentyNine
+
+    // else 30 day month - thirty
+
+    // else 31 day month - thirtyOne
+}
+
+// functions to run on page load
+function setUpPage() {
+    removeSelectDefaults();
+}
+
+// enable load event handlers
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
+}
